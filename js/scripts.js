43e80e8aca9df3258ff43a03f031300f89472ec2
glenbuck503 <glenbuck503@gmail.com>
2021-01-29 12:52:43 -0800

    add pizza picture to blank column

--- js/scripts.js
+++ js/scripts.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
     let custPizza = new Pizza (size, toppings);
     let total = custPizza.orderPrice();
     let pizzas = [];
-    
+    $(".pizzaPic").hide()
     $("#orderPrice").show()
     $("#total").html("The total cost for your order is:   $" + total + " . Thank you! Hope you enjoy!");
     $(pizzas.push(custPizza));
