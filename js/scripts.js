abf238d1603ffed1ad4606f3d9eb592834a88312
glenbuck503 <glenbuck503@gmail.com>
2021-01-29 09:33:21 -0800

    add orderPrice prototype and branching statements for user selected size

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,17 @@ function Pizza(size,toppings) {
   this.toppings = toppings;
 }
 
+Pizza.prototype.orderPrice = function() {
+  let orderTotal = 0;
+
+  if (this.size === "8") {
+    orderTotal += 8;
+  } else if (this.size === "10") {
+    orderTotal += 10;
+  } else if (this.size === "12") {
+    orderTotal += 12;
+  }
+  
+
+
+ 
